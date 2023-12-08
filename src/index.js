bffc46a60fa3ceb4f89169b0d209f4f2c511dad2
dc-andysign <andrei.baloiu@gmail.com>
2023-12-08 13:43:59 +0000

    Added start and build inside package json

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+// import App from './App'
+// import GlobalFonts from './assets/fonts/fonts'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <h1>HyProof GUI ( placeholder )</h1>
+    {/* <App /> */}
+    {/* <GlobalFonts /> */}
+  </React.StrictMode>
+)
