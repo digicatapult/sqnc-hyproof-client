fc643a23aeed2b3b5f84e3c22a0ba0efc2ae2702
Matthew Dean <matthew.dean@digicatapult.org.uk>
2024-01-16 09:05:21 +0000

    Feature/cert layout (#12)
    * Basic layout for certificate form
    
    * 0.0.8
    
    * Remove unused
    
    * format babelrc
    
    * Temporary move
    
    * Move back

---
+++ src/pages/Certificates.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Nav from './components/Nav'
+import Header from './components/Header'
+import CertificateForm from './components/CertificateForm'
+
+export default function Certificates() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <CertificateForm />
+    </>
+  )
+}
