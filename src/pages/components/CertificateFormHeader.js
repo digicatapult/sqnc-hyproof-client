3b12a56366134e0cb5ad29e752396fde564ee5a8
dc-andysign <andrei.baloiu@gmail.com>
2024-01-16 17:34:05 +0000

    Added div wrapper w min width.

--- src/pages/components/CertificateFormHeader.js
+++ src/pages/components/CertificateFormHeader.js
@@ -1,47 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Section } from '@digicatapult/ui-component-library' // H4
+import { Section } from '@digicatapult/ui-component-library'
 
 import BgImageSVG from '../../assets/images/molecules-bg-repeat.svg'
 
-export default function Test() {
+export default function CertificateFormHeader() {
   return (
     <>
       <GreenBgDiv>
-        <Section
-          background="#e4e4e4"
-          headingGap="0ch"
-          headingLevel={3}
-          headingSize="0em"
-          height="auto"
-          padding="1ch 1ch"
-          title=""
-          width="33%"
-        >
-          <UnorderedList>
-            <ListItem>
-              Hydrogen Producer: <strong>Heidi</strong>
-            </ListItem>
-            <ListItem>
-              Energy Producer: <strong>Emma</strong>
-            </ListItem>
-            <ListItem>
-              Regulator: <strong>Reginald</strong>
-            </ListItem>
-          </UnorderedList>
-          {/* <H4 headingLevel={1}>
-            <GrayText>Hydrogen Producer:</GrayText>
-            <BlackStrongText>Test</BlackStrongText>
-          </H4> */}
-        </Section>
+        <div style={{ minWidth: '200px' }}>
+          <Section
+            background="#e4e4e4"
+            headingGap="0ch"
+            headingLevel={3}
+            headingSize="0em"
+            height="auto"
+            padding="1ch 1ch"
+            title=""
+            width="33%"
+          >
+            <UnorderedList>
+              <ListItem>
+                Hydrogen Producer: <strong>Heidi</strong>
+              </ListItem>
+              <ListItem>
+                Energy Producer: <strong>Emma</strong>
+              </ListItem>
+              <ListItem>
+                Regulator: <strong>Reginald</strong>
+              </ListItem>
+            </UnorderedList>
+          </Section>
+        </div>
       </GreenBgDiv>
     </>
   )
 }
 
 const GreenBgDiv = styled.div`
-  background: #228077 url(${BgImageSVG}) repeat 50%;
+  background: #228077 url(${BgImageSVG}) repeat 25%;
 `
 
 const UnorderedList = styled.ul`
@@ -57,21 +55,3 @@ const ListItem = styled.li`
   font-weight: 100;
   font-size: 16px;
 `
-
-// const BlackStrongText = styled.span`
-//   color: #1a1a1a;
-//   font-family: Roboto;
-//   font-size: 16px;
-//   font-style: normal;
-//   font-weight: 400;
-//   // line-height: 0px; /* 0% */
-// `
-
-// const GrayText = styled.span`
-//   color: #7d7d7d;
-//   font-family: Roboto;
-//   font-size: 16px;
-//   font-style: normal;
-//   font-weight: 400;
-//   // line-height: 0px; /* 0% */
-// `
