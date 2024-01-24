d9a9e0fca2ab1c301f60e2d0c526595981db35eb
Paulius <paulius.michelevicius@gmail.com>
2024-01-24 15:16:33 +0000

    HYP-98: including the timeline component into the certificate page. (#19)
    * HYP-98: including the timeline component into the certificate page.
    
    * HYP-98: version bump.
    
    * HYP-98: forgot package-lock.
    
    * HYP-98: removing unused code.

--- src/pages/Certificates.js
+++ src/pages/Certificates.js
@@ -4,6 +4,29 @@ import Nav from './components/Nav'
 import Header from './components/Header'
 import CertificateForm from './components/CertificateForm'
 
+const timelineProps = {
+  name: 'UK-HYPROOF-0001',
+  disclaimer:
+    'Your certification status is dynamic and may change  over time. Always refer to this page for the most up-to-date status.',
+  items: [
+    {
+      title: 'Initiation',
+      message:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+      checked: true,
+      status: 'in progress',
+    },
+    {
+      title: 'Carbon Embodiment',
+      status: 'pending',
+    },
+    {
+      title: 'Issuance',
+      status: 'pending',
+    },
+  ],
+}
+
 export default function Certificates() {
   const userName = 'Heidi Heidi'
   const companyName = "Heidi's Hydroelectric Hydrogen"
@@ -11,7 +34,7 @@ export default function Certificates() {
     <>
       <Nav />
       <Header userFullName={userName} companyName={companyName} />
-      <CertificateForm />
+      <CertificateForm variant="hyproof" {...timelineProps} />
     </>
   )
 }
--- src/pages/components/CertificateForm.js
+++ src/pages/components/CertificateForm.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Grid } from '@digicatapult/ui-component-library'
+import { Grid, Timeline } from '@digicatapult/ui-component-library'
 
-export default function CertificateForm() {
+export default function CertificateForm(props) {
   return (
     <Form>
-      <Timeline area="timeline">timeline</Timeline>
+      <TimelineWrapper area="timeline">
+        <Timeline {...props}>
+          {props.items.map(({ message, ...rest }) => (
+            <Timeline.Item key={rest.title} {...props} {...rest}>
+              {message && <p>{message}</p>}
+            </Timeline.Item>
+          ))}
+        </Timeline>
+        <TimelineDisclaimer>{props.disclaimer}</TimelineDisclaimer>
+      </TimelineWrapper>
       <Grid.Panel area="main">
         <h1>Certificate Form</h1>
       </Grid.Panel>
@@ -21,14 +30,20 @@ const Form = styled.form`
   grid-area: 1 / 1 / -1 / -1;
 `
 
-const Timeline = styled(Grid.Panel)`
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  min-width: 400px;
-  max-height: 600px;
-  color: white;
+const TimelineWrapper = styled(Grid.Panel)`
+  max-width: 400px;
+  max-height: 100%;
+  padding: 20px 0px;
+  overflow: hidden;
   background: #0c3b38;
+`
+
+const TimelineDisclaimer = styled('div')`
+  padding: 50px 20px;
+  color: #33e58c;
+  opacity: 0.5;
+  font-size: 12px;
+  line-height: 20px; /* 166.667% */
 `
 
 const Sidebar = styled(Grid.Panel)`
