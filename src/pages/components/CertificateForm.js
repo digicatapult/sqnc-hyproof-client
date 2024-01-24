6c22ff48aa24a8720d8bf22673054c6b97e02791
dc-andysign <andrei.baloiu@gmail.com>
2024-01-24 10:23:08 +0000

    Added useCallback for CertificateForm and removed fragments.

--- src/pages/components/CertificateForm.js
+++ src/pages/components/CertificateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import styled from 'styled-components'
 
 import { Grid } from '@digicatapult/ui-component-library'
@@ -6,6 +6,8 @@ import { Grid } from '@digicatapult/ui-component-library'
 import CertificateInputFields from './CertificateInputFields'
 import CertificateFormHeader from './CertificateFormHeader'
 import CertificateActionsButtons from './CertificateActionsButtons'
+
+const useCallbackChVal = (set) => useCallback((e) => set(e.target.value), [set])
 
 export default function CertificateForm() {
   const [sdVal, setSdVal] = useState('2024-01-01')
@@ -15,12 +17,12 @@ export default function CertificateForm() {
   const [enVal, setEnVal] = useState('')
   const [szVal, setSzVal] = useState('')
 
-  const handleSdChgeVal = (e) => setSdVal(e.target.value)
-  const handleStChgeVal = (e) => setStVal(e.target.value)
-  const handleEdChgeVal = (e) => setEdVal(e.target.value)
-  const handleEtChgeVal = (e) => setEtVal(e.target.value)
-  const handleEnChgeVal = (e) => setEnVal(e.target.value)
-  const handleSzChgeVal = (e) => setSzVal(e.target.value)
+  const handleSdChgeVal = useCallbackChVal(setSdVal)
+  const handleStChgeVal = useCallbackChVal(setStVal)
+  const handleEdChgeVal = useCallbackChVal(setEdVal)
+  const handleEtChgeVal = useCallbackChVal(setEtVal)
+  const handleEnChgeVal = useCallbackChVal(setEnVal)
+  const handleSzChgeVal = useCallbackChVal(setSzVal)
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -32,31 +34,33 @@ export default function CertificateForm() {
       energyVal: enVal,
       sizeVal: szVal,
     }
-    alert('DATA:\n' + JSON.stringify(obj, null, 2)) // window.location.href = 'https://www.google.com'
+    alert('DATA:\n' + JSON.stringify(obj, null, 2))
   }
 
   return (
-    <Form action="" onSubmit={handleSubmit}>
+    <>
       <Timeline area="timeline">timeline</Timeline>
-      <Grid.Panel area="main">
-        <CertificateFormHeader />
-        <CertificateInputFields
-          sdVal={sdVal}
-          handleSdChgeVal={handleSdChgeVal}
-          stVal={stVal}
-          handleStChgeVal={handleStChgeVal}
-          edVal={edVal}
-          handleEdChgeVal={handleEdChgeVal}
-          etVal={etVal}
-          handleEtChgeVal={handleEtChgeVal}
-          enVal={enVal}
-          handleEnChgeVal={handleEnChgeVal}
-          szVal={szVal}
-          handleSzChgeVal={handleSzChgeVal}
-        />
-      </Grid.Panel>
-      <CertificateActionsButtons />
-    </Form>
+      <Form action="" onSubmit={handleSubmit}>
+        <Grid.Panel area="main">
+          <CertificateFormHeader />
+          <CertificateInputFields
+            sdVal={sdVal}
+            handleSdChgeVal={handleSdChgeVal}
+            stVal={stVal}
+            handleStChgeVal={handleStChgeVal}
+            edVal={edVal}
+            handleEdChgeVal={handleEdChgeVal}
+            etVal={etVal}
+            handleEtChgeVal={handleEtChgeVal}
+            enVal={enVal}
+            handleEnChgeVal={handleEnChgeVal}
+            szVal={szVal}
+            handleSzChgeVal={handleSzChgeVal}
+          />
+        </Grid.Panel>
+        <CertificateActionsButtons />
+      </Form>
+    </>
   )
 }
 
