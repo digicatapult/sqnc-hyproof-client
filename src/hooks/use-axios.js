d11f1b8b02739c4048b3f4489069cd336f867e98
dc-andysign <andrei.baloiu@gmail.com>
2024-01-29 13:23:57 +0000

    Cleand up comments in use axios.

--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -13,17 +13,6 @@ function useAxios(run = false, urlRun, bodyRun, methodRun, headersRun) {
     const options = body ? [url, body, { headers }] : [url, { headers }]
     headers = headers || { 'content-type': 'application/json' }
     method = method || (body ? 'post' : 'get')
-    // const body = {
-    //   energy_consumed_wh: 2000000,
-    //   production_start_time: '2024-01-25T10:00:00.000Z',
-    //   production_end_time: '2024-01-25T20:00:00.000Z',
-    //   regulator: 'Reginald',
-    //   energy_owner: 'Emma',
-    //   hydrogen_quantity_wh: 2000000,
-    // }
-    // const headers = { 'content-type': 'application/json' }
-
-    // const method = 'post'
 
     axios[method](...options)
       .then(async (res) => {
