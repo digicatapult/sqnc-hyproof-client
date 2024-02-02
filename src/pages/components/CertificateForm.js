134b1e38bb31f5a06b3533afbb0777e261618deb
dc-andysign <andrei.baloiu@gmail.com>
2024-02-02 17:04:41 +0000

    Cleaned up CertificateForm.

--- src/pages/components/CertificateForm.js
+++ src/pages/components/CertificateForm.js
@@ -6,8 +6,6 @@ import CertificateInputFields from './CertificateInputFields'
 import CertificateFormHeader from './CertificateFormHeader'
 import CertificateActionsButtons from './CertificateActionsButtons'
 
-// import useAxiosOld from '../../hooks/use-axios-old'
-
 import useAxios from '../../hooks/use-axios'
 
 const useCallbackChVal = (set) => useCallback((e) => set(e.target.value), [set])
@@ -21,15 +19,11 @@ export default function CertificateForm(props) {
   const [etVal, setEtVal] = useState('23:55')
   const [enVal, setEnVal] = useState('')
   const [szVal, setSzVal] = useState('')
-  // const [loading2, setLoading2] = useState(false)
+
   const [loading, setLoading] = useState(false)
   const [dataLocal, setDataLocal] = useState(null)
   const [dataChain, setDataChain] = useState(null)
   const [dataFinal, setDataFinal] = useState(null)
-
-  // const { error: errorLocal2, callApiFn: callApiFnLocal2 } = useAxiosOld()
-  // const { error: errorChain2, callApiFn: callApiFnChain2 } = useAxiosOld()
-  // const { error: errorFinal2, callApiFn: callApiFnFinal2 } = useAxiosOld()
 
   const { error: errorLocal, callApiFn: callApiFnLocal } = useAxios(false, [0])
   const { error: errorChain, callApiFn: callApiFnChain } = useAxios(false, [1])
@@ -41,72 +35,6 @@ export default function CertificateForm(props) {
   const handleEtChgeVal = useCallbackChVal(setEtVal)
   const handleEnChgeVal = useCallbackChVal(setEnVal)
   const handleSzChgeVal = useCallbackChVal(setSzVal)
-
-  // const handleSubmitStepLocalOld = useCallback(
-  //   (e) => {
-  //     e.preventDefault()
-  //     setLoading2(true)
-  //     const en = enVal ? (parseFloat(enVal) * 1000000) | 0 : 2000000
-  //     const sd = sdVal || '2024-01-01'
-  //     const st = stVal || '10:00'
-  //     const ed = edVal || '2024-01-01'
-  //     const et = etVal || '20:00'
-  //     const re = 'Reginald'
-  //     const eo = 'Emma'
-  //     const sz = szVal ? (parseFloat(szVal) * 1000000) | 0 : 3000000
-  //     const startTimestamp = `${sd}T${st}:00.000Z`
-  //     const endTimestamp = `${ed}T${et}:00.000Z`
-  //     const body = {
-  //       energy_consumed_wh: parseInt(en),
-  //       production_start_time: startTimestamp,
-  //       production_end_time: endTimestamp,
-  //       regulator: re,
-  //       energy_owner: eo,
-  //       hydrogen_quantity_wh: parseInt(sz),
-  //     }
-  //     const path = '/v1/certificate'
-  //     const url = `${origin}${path}`
-  //     callApiFnLocal2(url, body).then((d) => {
-  //       setDataLocal(d)
-  //       const heidiLocalId = d?.id
-  //       const body = {}
-  //       const path = `/v1/certificate/${heidiLocalId}/initiation`
-  //       const url = `${origin}${path}`
-  //       if (d.state === 'pending') {
-  //         callApiFnChain2(url, body).then((d) => {
-  //           setDataChain(d)
-  //           const heidiLocalId = d?.local_id
-  //           const path = `/v1/certificate/${heidiLocalId}`
-  //           const url = `${origin}${path}`
-  //           if (d?.state === 'submitted') {
-  //             ;(async function () {
-  //               let isFinalised = false
-  //               while (!isFinalised) {
-  //                 const d = await callApiFnFinal2(url)
-  //                 setDataFinal(d)
-  //                 if (d?.state === 'initiated') isFinalised = true
-  //                 await new Promise((resolve) => setTimeout(resolve, 1000))
-  //               }
-  //               setLoading2(false)
-  //             })()
-  //           }
-  //         })
-  //       }
-  //     })
-  //   },
-  //   [
-  //     enVal,
-  //     sdVal,
-  //     stVal,
-  //     edVal,
-  //     etVal,
-  //     szVal,
-  //     origin,
-  //     callApiFnLocal2,
-  //     callApiFnChain2,
-  //     callApiFnFinal2,
-  //   ]
-  // )
 
   const handleSubmitStepLocal = useCallback(
     (e) => {
