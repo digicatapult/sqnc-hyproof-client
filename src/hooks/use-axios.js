c5b9dff290ed0c2d434f8316be8d79f5a3330e0d
dc-andysign <andrei.baloiu@gmail.com>
2024-02-02 09:54:25 +0000

    Added cancelledState flag and cleanup useEffect function.

--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -6,34 +6,46 @@ function useAxios(run = false, urlRun, bodyRun, methodRun, headersRun) {
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
 
-  const callApiFn = useCallback(async (url, body, method, headers) => {
-    setLoading(true)
-    const options = body ? [url, body, { headers }] : [url, { headers }]
-    headers = headers || { 'content-type': 'application/json' }
-    method = method || (body ? 'post' : 'get')
+  const callApiFn = useCallback(
+    async (url, body, method, headers, cancelState) => {
+      setLoading(true)
+      headers = headers || { 'content-type': 'application/json' }
+      method = method || (body ? 'post' : 'get')
+      const options = body ? [url, body, { headers }] : [url, { headers }]
 
-    try {
-      const res = await axios[method](...options)
-      setData(res.data)
-      const { status } = res
-      if (status >= 400) {
-        setError(res)
-      } else {
-        setData(res.data)
-        setLoading(false)
-        return res.data
+      try {
+        const res = await axios[method](...options)
+        if (cancelState.cancelled == false) {
+          setData(res.data)
+          const { status } = res
+          if (status >= 400) {
+            setError(res)
+          } else {
+            setData(res.data)
+            setLoading(false)
+            return res.data
+          }
+        }
+      } catch (err) {
+        if (cancelState.cancelled == false) {
+          setError(err)
+          setLoading(false)
+        }
       }
-    } catch (err) {
-      setError(err)
-      setLoading(false)
-    }
-  }, [])
+    },
+    []
+  )
 
   useEffect(() => {
-    if (!run) {
-      return
+    const cancelState = { cancelled: false }
+
+    if (!run) return
+
+    callApiFn(urlRun, bodyRun, methodRun, headersRun, cancelState)
+
+    return () => {
+      cancelState.cancelled = true
     }
-    callApiFn(urlRun, bodyRun, methodRun, headersRun)
   }, [run, urlRun, bodyRun, methodRun, headersRun, callApiFn])
 
   return { data, error, loading, callApiFn }
