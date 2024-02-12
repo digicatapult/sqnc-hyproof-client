1f9d42209ffceddd042b6ae36b1270b0b6ac0ae1
dc-andysign <andrei.baloiu@gmail.com>
2024-02-12 09:50:16 +0000

    Bumped up version and uncommented router.

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,8 +1,8 @@
-// import { createBrowserRouter } from 'react-router-dom'
-//
-// import Certificates from '../pages/Certificates'
-//
-// export const router = createBrowserRouter([
-//   { path: '/', Component: Certificates },
-//   { path: '/certificates', Component: Certificates },
-// ])
+import { createBrowserRouter } from 'react-router-dom'
+
+import Certificates from '../pages/Certificates'
+
+export const router = createBrowserRouter([
+  { path: '/', Component: Certificates },
+  { path: '/certificates', Component: Certificates },
+])
