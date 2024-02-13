51c74c8cd45a251ff08a357c3e09a07606ef8624
dc-andysign <andrei.baloiu@gmail.com>
2024-02-13 19:16:18 +0000

    Removed comments from utils / Router

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,12 +1,3 @@
-// import { createBrowserRouter } from 'react-router-dom'
-//
-// import Certificates from '../pages/Certificates'
-//
-// export const router = createBrowserRouter([
-//   { path: '/', Component: Certificates },
-//   { path: '/certificates', Component: Certificates },
-// ])
-
 import React from 'react'
 
 import {
