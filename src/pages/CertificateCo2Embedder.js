649824d3060b0699302bc0854a25bd1ca10e1ef7
dc-andysign <andrei.baloiu@gmail.com>
2024-02-14 09:14:32 +0000

    Addec certificate embedder page.

---
+++ src/pages/CertificateCo2Embedder.js
@@ -0,0 +1,93 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { Grid } from '@digicatapult/ui-component-library'
+
+import Nav from './components/Nav'
+import Header from './components/Header'
+
+import { Context } from '../utils/Context'
+import { personas } from '../App'
+
+import { useParams } from 'react-router-dom'
+
+export default function CertificateCo2Embedder() {
+  const {
+    current,
+    currentId,
+    currentCommitmentSalt,
+    currentEnergyConsumedWh,
+    currentProductionStartTime,
+    currentProductionEndTime,
+  } = useContext(Context)
+  const persona = personas.find(({ id }) => id === current)
+  let { id } = useParams()
+  return (
+    <>
+      <Nav />
+      <Header userFullName={persona.name} companyName={persona.company} />
+      <LeftWrapper area="timeline"></LeftWrapper>
+      <MainWrapper>
+        <Grid.Panel area="main">
+          <Container>
+            <Text>
+              <h3>Cert Embed Id: {id}</h3>
+              <hr />
+              {current == 'heidi' && <>Switch2Emma</>}
+              {current == 'emma' && (
+                <code>
+                  cur id: {currentId} <br />
+                  currentCommitmentSalt: {currentCommitmentSalt} <br />
+                  currentEnergyConsumedWh: {currentEnergyConsumedWh} <br />
+                  currentProductionStartTime: {currentProductionStartTime}{' '}
+                  <br />
+                  currentProductionEndTime: {currentProductionEndTime} <br />
+                  TODO: embed the co2 data w/ <br />
+                  GET /v1/certificate ( get the latest that matches the above ){' '}
+                  <br />
+                  POST v1/certificate/$emma_local_id <br />
+                  POST v1/certificate/$emma_local_id/issuance
+                </code>
+              )}
+            </Text>
+          </Container>
+        </Grid.Panel>
+        <Sidebar area="sidebar"></Sidebar>
+      </MainWrapper>
+    </>
+  )
+}
+
+const LeftWrapper = styled(Grid.Panel)`
+  max-width: 400px;
+  max-height: 100%;
+  padding: 20px 0px;
+  overflow: hidden;
+  background: #0c3b38;
+`
+
+const MainWrapper = styled.div`
+  display: grid;
+  grid: subgrid / subgrid;
+  grid-area: 2 / 1 / -1 / -1;
+  overflow: hidden;
+  text-align: center;
+`
+
+const Sidebar = styled(Grid.Panel)`
+  align-items: center;
+  justify-items: center;
+  min-width: 340px;
+  color: white;
+  background: #0c3b38;
+`
+
+const Container = styled.div`
+  display: grid;
+  height: 100%;
+  grid-area: 1 / 1 / -1 / -1;
+`
+
+const Text = styled.h2`
+  margin: auto 5px;
+  text-align: center;
+`
