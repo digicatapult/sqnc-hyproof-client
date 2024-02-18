622b6318c176606092d807422055dbfc1b9e1eb0
dc-andysign <andrei.baloiu@gmail.com>
2024-02-18 11:35:04 +0000

    Added the new Router: / redirect to /certificate?create=true ; /certificate render Certificates or CertificatesViewer and CertificateManager for :id.

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useContext, useEffect } from 'react'
 
 import {
   RouterProvider,
@@ -6,14 +7,35 @@ import {
   createRoutesFromElements,
   Route,
   Outlet,
+  Navigate,
+  useParams,
+  useSearchParams,
+  useNavigate,
 } from 'react-router-dom'
 
+import { Context } from '../utils/Context'
+
+// import CertificateManager from '../pages/components/Certificate'
+import CertificateManager from '../pages/components/CertificateManager'
+
+// import Error404 from '../pages/Error404'
 import Certificates from '../pages/Certificates'
 import CertificatesViewAll from '../pages/CertificatesViewAll'
 import CertificateNotProvided from '../pages/CertificateNotProvided'
 import CertificateViewer from '../pages/CertificateViewer'
 import CertificateCo2Embedder from '../pages/CertificateCo2Embedder'
-import Error404 from '../pages/Error404'
+
+const CertificateViewerOrCreator = () => {
+  const create = useSearchParams()[0].get('create')
+  return create ? <Certificates /> : <CertificatesViewAll />
+}
+
+// const CertificateManager = () => {
+//   const { current } = useContext(Context)
+//   return <>{current != 'emma' ? <>CertificateViewer</> : <>Check_hasCo2</>}</>
+// }
+
+const Error404 = () => <>Error404</>
 
 export default function Routes() {
   return (
@@ -21,15 +43,11 @@ export default function Routes() {
       router={createBrowserRouter(
         createRoutesFromElements(
           <>
-            {/* TODO: The below should be renamed to CertificateCreator / Initiator */}
-            <Route path="/" element={<Certificates />} />
-            <Route path="/certificates" element={<CertificatesViewAll />} />
+            {/* TODO: Certificates should be renamed to CertificateCreator / Initiator */}
+            <Route path="/" element={<Navigate to="/certificate?create=y" />} />
             <Route path="/certificate" element={<Outlet />}>
-              <Route index element={<CertificateNotProvided />} />
-              <Route path=":id" element={<Outlet />}>
-                <Route index element={<CertificateViewer />} />
-                <Route path="embed" element={<CertificateCo2Embedder />} />
-              </Route>
+              <Route index element={<CertificateViewerOrCreator />} />
+              <Route path=":id" element={<CertificateManager />} />
             </Route>
             <Route path="*" element={<Error404 />} />
           </>
