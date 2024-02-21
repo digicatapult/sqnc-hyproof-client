c472572da02fe6aea7124a142e1896832acf9895
dc-andysign <andrei.baloiu@gmail.com>
2024-02-21 19:35:28 +0000

    Added CertificateViewHeader and the css for it.

--- src/pages/CertificateViewer.js
+++ src/pages/CertificateViewer.js
@@ -13,6 +13,93 @@ import { useParams } from 'react-router-dom'
 
 import useAxios from '../hooks/use-axios'
 
+import ApprovalSealLargeSVG from '../assets/images/approval-seal-large.svg'
+
+const CertificateViewHeader = () => {
+  return (
+    <HeaderContainerUnderlined>
+      <HeaderContainer>
+        <HeaderImage />
+        <HeaderTextHeadings>
+          <H1>CERTIFICATE</H1>
+          <H2>HYDROGEN PRODUCTION</H2>
+          <H3>HYDROGEN PRODUCTION</H3>
+        </HeaderTextHeadings>
+        <HeaderImage />
+      </HeaderContainer>
+    </HeaderContainerUnderlined>
+  )
+}
+
+const HeaderContainerUnderlined = styled.div`
+  border-bottom: 2px solid #27847a;
+  width: 100%;
+`
+
+const HeaderContainer = styled.div`
+  margin: 66px auto 20px auto;
+  width: 100%;
+`
+
+const HeaderImage = styled.div`
+  display: inline-block;
+  padding: 10px;
+  width: 16.5%;
+  height: 100px;
+  border: 1px solid black;
+
+  background: transparent url(${ApprovalSealLargeSVG}) no-repeat center;
+  background-size: contain;
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
+`
+
+const HeaderTextHeadings = styled.div`
+  display: inline-block;
+  padding: 0 10px;
+  width: 66%;
+  border: 1px solid black;
+
+  // min-width: 500px;
+  // min-width: 433px;
+
+  color: #27847a;
+`
+
+const H1 = styled.h1`
+  display: block;
+  font-family: Roboto;
+  font-size: 57px;
+  font-style: normal;
+  font-weight: 300;
+  // line-height: 0px;
+  margin-top: 0px;
+  margin-bottom: -9px;
+`
+
+const H2 = styled.h2`
+  display: block;
+  font-family: Roboto;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: bold;
+  // line-height: 0px;
+  margin-top: -9px;
+  margin-bottom: -3px;
+`
+
+const H3 = styled.h3`
+  display: block;
+  font-family: Roboto;
+  font-size: 18.5px;
+  font-style: normal;
+  font-weight: 300;
+  margin-top: -2px;
+  // line-height: 0px;
+`
+
 export default function CertificateViewer() {
   const { current } = useContext(Context)
   const persona = personas.find(({ id }) => id === current)
@@ -67,12 +154,12 @@ export default function CertificateViewer() {
                       ['div-double', 'div-double'],
                       ['div-left', 'div-right'],
                     ]}
-                    rows={['40px', '60px']}
+                    rows={['auto', 'auto']}
                     columns={['1fr', '1fr']}
                     gap="15px"
                   >
                     <Grid.Panel area="div-double">
-                      CertificateViewHeader
+                      <CertificateViewHeader />
                     </Grid.Panel>
                     <Grid.Panel area="div-left">
                       CertificateViewOwnership
@@ -133,7 +220,7 @@ const Paper = styled.div`
   color: #000000;
   background: #ffffff;
   overflow: hidden;
-  &:before {
+  &::before {
     content: '';
     position: absolute;
     top: 0;
@@ -145,7 +232,7 @@ const Paper = styled.div`
     display: block;
     width: 0;
   }
-  &:after {
+  &::after {
     content: '';
     position: absolute;
     top: 0;
