2bcee97bb92667f0f911dc87c95c28c0461f79e2
dc-andysign <andrei.baloiu@gmail.com>
2024-02-22 16:55:27 +0000

    Added CertificateViewOwnership component that renders the id and the heading text.

--- src/pages/CertificateViewer.js
+++ src/pages/CertificateViewer.js
@@ -15,6 +15,115 @@ import useAxios from '../hooks/use-axios'
 
 import CertificateViewHeader from './components/CertificateViewHeader'
 
+import LockerSvg from '../assets/images/locker-icon.svg'
+import SealSvg from '../assets/images/approval-seal-small.svg'
+
+const DivLock = styled.div`
+  text-align: left;
+`
+
+const SpanLock = styled.span`
+  width: 100%;
+  position: relative;
+  color: #4e9a91;
+  font-family: Roboto;
+  font-size: 18.4px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  margin-left: 45px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: -45px;
+    margin-top: -15px;
+    width: 30px;
+    height: 30px;
+    margin-right: 15px;
+    border-radius: 50%;
+    background: #1f7d74 url(${LockerSvg}) no-repeat;
+  }
+`
+
+const DivRounded = styled.div`
+  border-radius: 10px;
+  background: #27847a;
+  color: #ffffff;
+  padding: 20px;
+  margin: 8px 0;
+  text-align: left;
+
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+`
+
+const Div = styled.div`
+  line-height: 30px;
+  margin: 20px 0 0 0;
+`
+
+const Hr = styled.hr`
+  border-style: solid;
+  margin: 40px 0 20px 0;
+`
+
+const DivImageLeft = styled.div`
+  width: 100%;
+  position: relative;
+  color: #ffffff;
+  font-family: Roboto;
+  font-size: 18.4px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 50px;
+  padding-left: 44px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0px;
+    margin-top: -25px;
+    width: 34px;
+    height: 50px;
+    background: transparent url(${SealSvg}) no-repeat;
+  }
+`
+
+const Small = styled.small`
+  color: #94c0bc;
+  font-family: Roboto;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px;
+  display: block;
+`
+
+const CertificateViewOwnership = ({ hOwner, eOwner }) => {
+  return (
+    <>
+      <DivLock>
+        <SpanLock>UK-HYPROOF-0001</SpanLock>
+      </DivLock>
+      <DivRounded>
+        <Div>Hydrogen Owner: {hOwner}</Div>
+        <Div>Energy Owner: {eOwner}</Div>
+        <Hr />
+        <DivImageLeft>
+          <Div>LCHS</Div>
+          <Small>Adherence to government standards</Small>
+        </DivImageLeft>
+      </DivRounded>
+    </>
+  )
+}
+
 export default function CertificateViewer() {
   const { current } = useContext(Context)
   const persona = personas.find(({ id }) => id === current)
@@ -66,21 +175,25 @@ export default function CertificateViewer() {
                 <>
                   <Grid
                     areas={[
-                      ['div-double', 'div-double'],
-                      ['div-left', 'div-right'],
+                      ['div-header', 'div-header', 'div-header'],
+                      ['div-ownership', 'div-details', 'div-details'],
                     ]}
                     rows={['auto', 'auto']}
-                    columns={['1fr', '1fr']}
+                    columns={['1fr', '2fr']}
                     gap="15px"
                   >
-                    <Grid.Panel area="div-double">
+                    <Grid.Panel area="div-header">
                       <CertificateViewHeader id={id} />
                     </Grid.Panel>
-                    <Grid.Panel area="div-left">
-                      CertificateViewOwnership
+                    <Grid.Panel area="div-ownership">
+                      <CertificateViewOwnership
+                        hOwner={data?.hydrogen_owner}
+                        eOwner={data?.energy_owner}
+                      />
                     </Grid.Panel>
-                    <Grid.Panel area="div-right">
+                    <Grid.Panel area="div-details">
                       CertificateViewDetails
+                      <small>{data && JSON.stringify(data, null, 2)}</small>
                     </Grid.Panel>
                   </Grid>
                 </>
