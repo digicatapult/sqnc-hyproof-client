91da9fe23b933756163e76a7fa4df00bafa853c6
dc-andysign <andrei.baloiu@gmail.com>
2024-02-28 13:42:09 +0000

    Redone the fetchLastCert then co2PostIfNeeded login so that the interval loop start after post.

--- src/pages/CertificateViewer.js
+++ src/pages/CertificateViewer.js
@@ -101,18 +101,36 @@ export default function CertificateViewer() {
     }
 
     // If Emma then post co2 if it hasn't got co2, as in, if no embedded co2 from fetch
-    curPersona === 'emma' && fetchLatestCert().then((c) => co2PostIfNeeded(c))
+    curPersona === 'emma' &&
+      fetchLatestCert().then((c) => {
+        // eslint-disable-next-line no-console
+        console.log('done fetchLatestCert')
+        co2PostIfNeeded(c).then(() => {
+          // eslint-disable-next-line no-console
+          console.log('done co2PostIfNeeded')
+          intervalId = setInterval(async () => {
+            const latestCert = await fetchLatestCert()
+            if (JSON.stringify(latestCert) != JSON.stringify(latest.current)) {
+              latest.current = latestCert
+              setData(latestCert)
+            }
+            // eslint-disable-next-line no-console
+            console.log('d', Math.random(), JSON.stringify(latest.current))
+          }, 2 * 1000)
+        })
+      })
 
     // Fetch every few secs ( TODO: add delay ? )
-    intervalId = setInterval(async () => {
-      const latestCert = await fetchLatestCert()
-      // eslint-disable-next-line no-console
-      console.log('dataCache', Math.random(), JSON.stringify(latest.current))
-      if (JSON.stringify(latestCert) != JSON.stringify(latest.current)) {
-        latest.current = latestCert
-        setData(latestCert)
-      }
-    }, 2 * 1000)
+    curPersona !== 'emma' &&
+      (intervalId = setInterval(async () => {
+        const latestCert = await fetchLatestCert()
+        if (JSON.stringify(latestCert) != JSON.stringify(latest.current)) {
+          latest.current = latestCert
+          setData(latestCert)
+        }
+        // eslint-disable-next-line no-console
+        console.log('dataCache', Math.random(), JSON.stringify(latest.current))
+      }, 2 * 1000))
 
     // Cleanup the interval on unmount
     return () => {
