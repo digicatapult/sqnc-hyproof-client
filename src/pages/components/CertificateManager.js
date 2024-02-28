c196c940f40184f2df25d2dd425fed72362f903f
dc-andysign <andrei.baloiu@gmail.com>
2024-02-28 13:31:24 +0000

    Cleaned up cert manager.

--- src/pages/components/CertificateManager.js
+++ src/pages/components/CertificateManager.js
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
 import CertificateViewer from '../CertificateViewer'
 
 export default function CertificateManager() {
