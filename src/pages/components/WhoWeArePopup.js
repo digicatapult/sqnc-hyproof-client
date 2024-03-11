5c67e0d62a3b57b444756d25fd5e284c0d245b95
dc-andysign <andrei.baloiu@gmail.com>
2024-03-11 22:13:56 +0000

    Added h1 , sub and ul

--- src/pages/components/WhoWeArePopup.js
+++ src/pages/components/WhoWeArePopup.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 import { Section, Grid } from '@digicatapult/ui-component-library'
 
@@ -12,29 +12,77 @@ export default function WhoWeArePopup() {
           <Section
             headingLevel={2}
             title=""
-            padding="50px 100px"
+            padding="50px 100px 5px 100px"
             margin="0 -1em"
             background="#27847a"
             headingSize="0em"
           >
-            <h3>Title</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Test
+            <H1>Title</H1>
+            <Sub>Sub</Sub>
           </Section>
         </Grid.Panel>
         <Grid.Panel area="bottom" padding="50px 100px">
           <Section
             headingLevel={2}
             title=""
-            padding="50px 100px"
+            padding="50px 100px 40px 100px"
             margin="0 -1em"
             background="#ffffff"
             headingSize="0em"
           >
-            <h3>Title</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Test
+            <H2>Title</H2>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Test
+              <Ul>
+                <Li>
+                  <strong>THE FIRST BULLET POINT IN BOLD:</strong>
+                  <br />
+                  Text with indent
+                </Li>
+                <Li>
+                  <strong>THE SECOND ONE WITH BOLD:</strong>
+                  <br />
+                  More text
+                </Li>
+              </Ul>
+            </Text>
           </Section>
         </Grid.Panel>
       </Grid>
     </Section>
   )
 }
+
+const H1 = styled.h1`
+  color: #ffffff;
+  font: normal 500 26px/0 Roboto;
+
+  margin: 25px 0;
+`
+
+const H2 = styled.h1`
+  color: #1a1a1a;
+  font: normal 700 16px/0 Roboto;
+
+  margin: 25px 0;
+`
+
+const Sub = styled.p`
+  color: #94c0bc;
+  font: normal 500 12px/0 Roboto;
+  margin: 40px 0 40px 0;
+`
+
+const Text = styled.p`
+  color: #808080;
+
+  font: normal 500 14px/24px Roboto;
+`
+
+const Ul = styled.ul`
+  padding-inline-start: 10px;
+`
+
+const Li = styled.li`
+  margin-bottom: 5px;
+`
