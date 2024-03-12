bbf95c5b9de25cfa71239bee1fd4a127f0f0dace
dc-andysign <andrei.baloiu@gmail.com>
2024-03-12 00:22:39 +0000

    Added close button.

--- src/pages/components/WhoWeArePopup.js
+++ src/pages/components/WhoWeArePopup.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from '@digicatapult/ui-component-library'
 
 import styled from 'styled-components'
 
 import { Section, Grid } from '@digicatapult/ui-component-library'
 
-export default function WhoWeArePopup() {
+export default function WhoWeArePopup({ handleClick }) {
   return (
     <Section headingLevel={1} title="" padding="0em 1em" headingSize="0em">
       <Grid areas={[['top'], ['bottom']]} rows={['auto']} columns={['auto']}>
@@ -12,13 +13,16 @@ export default function WhoWeArePopup() {
           <Section
             headingLevel={2}
             title=""
-            padding="50px 100px 5px 100px"
+            padding="70px 100px 5px 100px"
             margin="0 -1em"
             background="#27847a"
             headingSize="0em"
           >
-            <H1>Title</H1>
-            <Sub>Sub</Sub>
+            <H1>What We Do</H1>
+            <Sub>
+              Learn more about our low carbon hydrogen certification initiative,
+              HyProof.
+            </Sub>
           </Section>
         </Grid.Panel>
         <Grid.Panel area="bottom" padding="50px 100px">
@@ -30,22 +34,53 @@ export default function WhoWeArePopup() {
             background="#ffffff"
             headingSize="0em"
           >
-            <H2>Title</H2>
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Test
+              Digital Catapult is a UK government-backed organisation with over
+              a decade of experience in accelerating the adoption of advanced
+              digital technologies within various industries. Our mission is to
+              accelerate industry adoption of advanced digital technologies,
+              driving growth in the UK economy. We operate with a vendor-neutral
+              and not-for-profit status.
+            </Text>
+            <H2>HyProof - Transforming Hydrogen Certification</H2>
+            <Text>
+              The HyProof certification addresses a critical need within the
+              hydrogen sector: a robust and reliable certification system for
+              verifying the green credentials of hydrogen production, in the
+              emerging hydrogen economy. Developed in response to both industry
+              concerns and government consultations, HyProof showcases the
+              potential of Distributed Ledger Technology to create a
+              decentralised, shared public good solution for the whole of UK.
+            </Text>
+            <H2>Why does HyProof use Distributed Ledger Technology (DLT)?</H2>
+            <Text>
               <Ul>
                 <Li>
-                  <strong>THE FIRST BULLET POINT IN BOLD:</strong>
+                  <strong>Distributed & Secure</strong>:
                   <br />
-                  Text with indent
+                  HyProof DLT reflects the multi-actor nature of the hydrogen
+                  ecosystem, eliminating single points of control or data loss
+                  and ensuring cryptographic integrity of all information.
                 </Li>
                 <Li>
-                  <strong>THE SECOND ONE WITH BOLD:</strong>
+                  <strong>Empowering Transparency</strong>:
                   <br />
-                  More text
+                  Empowers individual entities of the multi-actor ecosystem with
+                  greater control and sovereignty over their data, fostering
+                  trust and transparency within the system.
+                </Li>
+                <Li>
+                  <strong>Regulatory Governance</strong>:
+                  <br />
+                  HyProof DLT reflects the multi-actor nature of the hydrogen
+                  ecosystem, eliminating single points of control or data loss
+                  and ensuring cryptographic integrity of all information.
                 </Li>
               </Ul>
             </Text>
+            <CloseButton variant="roundedPronounced" onClick={handleClick}>
+              Click
+            </CloseButton>
           </Section>
         </Grid.Panel>
       </Grid>
@@ -76,7 +111,7 @@ const Sub = styled.p`
 const Text = styled.p`
   color: #808080;
 
-  font: normal 500 14px/24px Roboto;
+  font: normal 500 14px/24.5px Roboto;
 `
 
 const Ul = styled.ul`
@@ -84,5 +119,18 @@ const Ul = styled.ul`
 `
 
 const Li = styled.li`
-  margin-bottom: 5px;
+  margin-bottom: 5.5px;
+`
+
+const CloseButton = styled(Button)`
+  min-height: 40px;
+  min-width: 180px;
+  margin-top: 30px;
+  font: normal 500 15px/15px Roboto;
+  color: #33e58c;
+  border: 1px solid #2fe181;
+  background: #124338;
+  &:hover {
+    opacity: 0.6;
+  }
 `
