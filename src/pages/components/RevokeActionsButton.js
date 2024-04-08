16b1e2f91b1a366071fd0d971ad90ac5d6863c1b
dc-andysign <andrei.baloiu@gmail.com>
2024-04-08 18:42:11 +0100

    Added full revocation jsx and css

--- src/pages/components/RevokeActionsButton.js
+++ src/pages/components/RevokeActionsButton.js
@@ -6,11 +6,11 @@ import ReasonsPopup from './ReasonsPopup'
 
 // ReasonsViewPopup(imports)
 import { Section } from '@digicatapult/ui-component-library'
-// import WarningSignSvg from '../../assets/images/warning-sign.svg'
 import WarningSignSvg from '../../assets/images/warning-sign-icon.svg'
 
 // ReasonsViewPopup
 const ReasonsViewPopup = function ({ handleCancel }) {
+  const email = 'reginald@hydrogenregulator.org.uk'
   return (
     <>
       <Section
@@ -37,9 +37,34 @@ const ReasonsViewPopup = function ({ handleCancel }) {
         background="#ffffff"
         headingSize="0em"
       >
-        Body
-        <hr />
-        <button onClick={handleCancel}>Close</button>
+        <TextSection>
+          <TextTitle>Reason(s) for Revocation:</TextTitle>
+          <Ol>
+            <Li>
+              Data Error: Incorrect production date/quantity source information
+            </Li>
+            <Li>
+              Certification Misrepresentation: False claims about production
+              process
+            </Li>
+            <Li>Non-Compliance: Repeated certificate discrepancies</Li>
+            <Li>
+              Other: Failure to meet new safety protocols implemented on
+              03/02/2024 (regulation change).
+            </Li>
+          </Ol>
+        </TextSection>
+        <TextSection>
+          <TextTitle>What to Do:</TextTitle>
+          <Text>
+            Please contact the regulatory body at{' '}
+            <a href="mailto:{email}">{email}</a> for for more information on
+            certificate revocation procedures to discuss next steps.
+          </Text>
+        </TextSection>
+        <CloseButton variant="roundedPronounced" onClick={handleCancel}>
+          Close
+        </CloseButton>
       </Section>
     </>
   )
@@ -160,9 +185,6 @@ const TitleWarning = styled.div`
   font-weight: 500;
   line-height: 15px;
 
-  // margin-left: 70px;
-  // text-indent: -70px;
-
   &::before {
     content: '';
     position: absolute;
@@ -179,12 +201,61 @@ const TitleWarning = styled.div`
 const SubtitleWarning = styled.div`
   color: #8cb9b3;
 
-  // margin-left: 70px;
-  // padding-left: 70px;
-
   font-family: Roboto;
   font-size: 12px;
   font-style: normal;
   font-weight: 500;
   line-height: 24px;
+`
+
+const TextSection = styled.div`
+  text-align: left;
+  padding: 5px 0;
+  margin-bottom: 20px;
+`
+
+const TextTitle = styled.div`
+  padding: 15px 0;
+  color: #1a1a1a;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 14px;
+`
+
+const Text = styled.div`
+  color: #859d9b;
+
+  & a {
+    color: #859d9b;
+  }
+  & a:hover {
+    color: #1a1a1a;
+  }
+`
+
+const Ol = styled.ol`
+  margin-block: 0px;
+  line-height: 24px;
+  padding-left: 25px;
+  padding-inline-start: 25px;
+`
+
+const Li = styled.li`
+  margin-block-end: 5.5px;
+  color: #859d9b;
+`
+
+const CloseButton = styled(Button)`
+  min-height: 40px;
+  min-width: 180px;
+  margin-top: 30px;
+  font: normal 500 1rem Roboto;
+  color: #33e58c;
+  border: 1px solid #2fe181;
+  background: #124338;
+  &:hover {
+    opacity: 0.6;
+  }
 `
