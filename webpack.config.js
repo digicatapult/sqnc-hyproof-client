839190cc4660c6ff8fd53f3255a8d54404d37f9d
dc-andysign <andrei.baloiu@gmail.com>
2024-05-02 13:19:13 +0300

    Removed comments

--- webpack.config.js
+++ webpack.config.js
@@ -74,10 +74,7 @@ module.exports = (vars) => {
         analyzerMode: 'disabled', // disable analyser. Replace with "server" or "static" to render analyser
       }),
     ],
-    externals: { // externals object should be empty to include React & ReactDOM in the bundle making the GUI available offline
-      // react: 'React',
-      // 'react-dom': 'ReactDOM',
-    },
+    externals: {},
   }
 }
 
